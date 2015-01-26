34c71b4824ff4fd17f93686c1f9039769df642e9
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2015-01-26 17:09:58 +0100

    removed trws stuff

--- inferno-externals/trws/trws-prefix/src/trws-stamp/verify-trws.cmake
+++
@@ -1,4 +0,0 @@
-set(file "/home/tbeier/src/inferno/inferno-externals/trws/trws-prefix/src/TRW_S-v1.3.zip")
-message(STATUS "verifying file...
-     file='${file}'")
-message(STATUS "verifying file... warning: did not verify file - no URL_HASH specified?")
