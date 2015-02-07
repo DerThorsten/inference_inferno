99ee3c4fa5ff52109a02cb8bd787206a5c56a1c9
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2015-02-07 02:41:03 +0100

    work in prog.

--- include/inferno/model/view_submodel.hxx
+++ include/inferno/model/view_submodel.hxx
@@ -1,7 +1,7 @@
 #ifndef INFERNO_MODEL_VIEW_SUBMODEL_HXX
 #define INFERNO_MODEL_VIEW_SUBMODEL_HXX
 
-
+#include <cstdint> 
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
@@ -10,11 +10,119 @@
 
 #include "inferno/inferno.hxx"
 #include "inferno/value_tables/base_discrete_value_table.hxx"
-#include "inferno/model/base_discrete_model.hxx"
+#include "inferno/model/base_discrete_factor.hxx"
 #include "inferno/model/base_discrete_model.hxx"
 
+
 namespace inferno{
+
+
 namespace models{
+
+
+
+
+namespace detail_view_submodel{
+
+    template<class VIEW_SUBMODEL>
+    class FactorBase : public inferno::DiscreteFactorBase< FactorBase<VIEW_SUBMODEL> >{
+    public:
+        virtual ~FactorBase(){}
+        virtual Vi vi(const size_t d) const = 0;
+        virtual const inferno::value_tables::DiscreteValueTableBase * valueTable()const=0;
+    };
+
+    template<class VIEW_SUBMODEL>
+    class FullyIncludedFactor :  public FactorBase<VIEW_SUBMODEL>
+    {
+    private:
+        typedef typename VIEW_SUBMODEL::BaseModelFactorProxy BaseModelFactorProxy;
+    public:
+        FullyIncludedFactor(const VIEW_SUBMODEL & viewSubmodel, const Fi baseFi)
+        :   viewSubmodel_(&viewSubmodel),
+            baseModelFactor_(viewSubmodel.baseModel()[baseFi]){
+
+        }
+
+        virtual Vi vi(const size_t d) const {
+            return viewSubmodel_->baseViToVi(baseModelFactor_->vi(d));
+        }
+        virtual const inferno::value_tables::DiscreteValueTableBase * valueTable()const{
+            return baseModelFactor_->valueTable();
+        }
+    private:
+        const VIEW_SUBMODEL * viewSubmodel_;
+        const BaseModelFactorProxy baseModelFactor_;
+    };
+
+    template<class BASE_MODEL>
+    class ViMapping{
+
+    public:
+        typedef BASE_MODEL BaseModel;
+        ViMapping(const BaseModel & baseModel)
+        :   notInSubmodelId_(baseModel.maxVarId()+1),
+            baseViToVi_(baseModel,notInSubmodelId_),
+            viToBaseVi_(),
+            nVar_(0){
+
+        }
+
+        template<class BASE_VI_ITER>
+        void setSubmodelVariables(
+            BASE_VI_ITER baseViIter,
+            BASE_VI_ITER baseViEnd
+        ){
+            // forward and backward mapping 
+            // between vi and baseVi
+            nVar_ = 0 ;
+            for( ; baseViIter!=baseViEnd; ++baseViIter, ++nVar_){
+                const Vi baseVi = *baseViIter;
+                baseViToVi_[baseVi] = nVar_;
+                viToBaseVi_[nVar_] = baseVi;
+            }
+        }
+
+
+        Vi nVariables()const{
+            return nVar_;
+        }
+        Vi maxVarId()const{
+            return nVar_-1;
+        }
+        Vi minVarId()const{
+            return 0;
+        }
+        Vi baseViToVi(const Vi baseVi)const{
+            return baseViToVi_[baseVi];
+        }
+        Vi viToBaseVi(const Vi vi)const{
+            return viToBaseVi_[vi];
+        }
+
+        bool isInSubmodel(const Vi baseVi)const{
+            // \todo buffer max id
+            return baseViToVi_[baseVi] < nVar_ ;
+        }
+
+    private:
+        typedef typename BASE_MODEL:: template VariableMap<Vi> BaseModelViMap;
+        Vi notInSubmodelId_;
+        BaseModelViMap baseViToVi_;
+        std::vector<Vi> viToBaseVi_;
+        Vi nVar_;
+    };
+
+}
+
+
+
+
+
+
+
+
+
 /** \brief view submodel for a few variables.
     
     This class is designed for the following use case.
@@ -22,15 +130,30 @@ namespace models{
     from  BASE_MODEL (one submodel at the time).
 
 */
-template<class BASE_MODEL>
+template<class BASE_MODEL, class BASE_MODEL_FACTORS_OF_VARIABLES>
 class ViewSubmodel :
-public DiscreteGraphicalModelBase< ViewSubmodel<BASE_MODEL> > {
+    public DiscreteGraphicalModelBase< ViewSubmodel<BASE_MODEL, BASE_MODEL_FACTORS_OF_VARIABLES> > {
+private: 
+    typedef ViewSubmodel<BASE_MODEL, BASE_MODEL_FACTORS_OF_VARIABLES> Self;
+    enum FactorIncluding{
+        NotIncluded = 0,
+        PartialIncluded = 1,
+        FullyIncluded = 2
+    };
+
+
+    typedef detail_view_submodel::FactorBase<Self> FactorBase;
+    typedef detail_view_submodel::FullyIncludedFactor<Self> FullyIncludedFactor;
 
 public:
+    typedef typename BASE_MODEL::FactorProxy BaseModelFactorProxy;
+
+    typedef const FactorBase * FactorProxy;
     const static bool SortedVariableIds = true;
     const static bool SortedFactorIds = true;
-    typedef boost::counting_iterator<Vi> VariableIdIter;
 
+    typedef boost::counting_iterator<Vi> VariableIdIter;
+    typedef boost::counting_iterator<Fi> FactorIdIter;
     // typedefs of adapter API
 
     /** \var typedef BaseModel 
@@ -40,17 +163,34 @@ public:
     typedef typename BaseModel:: template VariableMap<Vi> BaseModelViMap;
     typedef typename BaseModel:: template VariableMap<DiscreteLabel> BaseModelLabelMap;
 
-    ViewSubmodel(const BASE_MODEL & baseModel)
+
+    typedef typename BaseModel:: template VariableMap<FactorIncluding> IncludedFactorMap;
+
+    ViewSubmodel(const BASE_MODEL & baseModel, const BASE_MODEL_FACTORS_OF_VARIABLES & baseModelFactorsOfVariables)
     :   baseModel_(baseModel),
-        baseViToVi_(baseModel),
+        baseModelFacOfVars_(baseModelFactorsOfVariables),
+        baseViToVi_(baseModel, baseModel.maxVarId()+1),
         viToBaseVi_(baseModel.nVariables()),
         baseModelLabels_(baseModel),
-        nVar_(0){
+        baseFiIncluding_(baseModel, NotIncluded),
+        includedFactors_(baseModel.nFactors(),0),
+        includedFactorsStorageIndex_(baseModel.nFactors(),0),
+        nVar_(0),
+        nFac_(0){
 
     }
 
 
     // adapter API
+
+    /** \brief set which variables from the base model 
+        are in the submodel.
+
+    */
+    template<class T>
+    void setSubmodelVariables(std::initializer_list<T> viList){
+        return setSubmodelVariables(viList.begin(), viList.end());
+    }
 
     /** \brief set which variables from the base model 
         are in the submodel.
@@ -62,11 +202,50 @@ public:
         BASE_VI_ITER baseViIter,
         BASE_VI_ITER baseViEnd
     ){
+        // forward and backward mapping 
+        // between vi and baseVi
         nVar_ = 0 ;
-        for( ; baseViIter!=baseViEnd; ++baseViIter, ++nVar_){
+        for( ; baseViIter!=baseViEnd; ++baseViIter){
             const Vi baseVi = *baseViIter;
             baseViToVi_[baseVi] = nVar_;
             viToBaseVi_[nVar_] = baseVi;
+            ++nVar_;
+        }
+
+        for(Vi vi=0; vi<nVar_; ++vi){
+            const Vi baseVi = viToBaseVi(vi);
+
+            // iterate over all factors for baseVi
+            for(const auto & baseFi : baseModelFacOfVars_[baseVi]){
+                std::cout<<"baseFi"<<baseFi<<"\n";
+                if(baseFiIncluding_[baseFi]==NotIncluded){
+                    INFERNO_ASSERT_OP(baseFi,<,baseModel_.maxFactorId()+1);
+                    const auto factor = baseModel_[baseFi];
+                    const auto arity = factor->arity();
+                    // two cases can happen
+                    // -factor is partial included 
+                    //   (not all vis of factors are included in this model)
+                    // -factor is total included
+                    //   (all vis are in included  )
+                    bool fullyIncluded = true;
+                    for(size_t a=0; a<arity; ++a){
+                        const auto baseVi = factor->vi(a);
+                        if(!isInSubmodel(baseVi)){
+                            fullyIncluded = false;
+                            break;
+                        }
+                    }
+                    if(fullyIncluded){
+                        baseFiIncluding_[baseFi] = FullyIncluded;
+
+                    }
+                    else{
+                        baseFiIncluding_[baseFi] = PartialIncluded;
+                    }
+                }
+                includedFactors_[nFac_] = baseFi;
+                ++nFac_;
+            }
         }
     }
 
@@ -77,11 +256,20 @@ public:
     VariableIdIter variableIdsEnd()const{
         return VariableIdIter(nVar_);
     }
+    FactorIdIter factorIdsBegin()const{
+        return FactorIdIter(0);
+    }
 
+    FactorIdIter factorIdsEnd()const{
+        return FactorIdIter(nFac_);
+    }
     LabelType nLabels(const Vi variabeId)const{
         return baseModel_.nLabels(this->viToBaseVi(variabeId));
     }
 
+    FactorProxy operator[](const Fi fi) const {
+        
+    }
 
     /** \brief get variable index of this
         from a variable index of the base model
@@ -108,13 +296,44 @@ public:
         return viToBaseVi_[vi];
     }
 
+    bool isInSubmodel(const Vi baseVi)const{
+        // \todo buffer max id
+        return baseViToVi_[baseVi] < nVar_ ;
+    }
+
+
+    FactorIncluding baseFiIncluding(const Fi baseFi)const{
+        return includedFactors_[baseFi];
+    }
+
+
+
+    const BaseModel & baseModel()const{
+        return baseModel_;
+    }
+
+
+    /** \brief get the baseModel label map
+    */
+    BaseModelLabelMap & baseModelLabelMap(){
+        return  baseModelLabels_;
+    }
 
 private:
     const BASE_MODEL & baseModel_;
+    const BASE_MODEL_FACTORS_OF_VARIABLES & baseModelFacOfVars_;
     BaseModelViMap baseViToVi_;
     std::vector<Vi> viToBaseVi_;
     BaseModelLabelMap baseModelLabels_;
+    IncludedFactorMap baseFiIncluding_;
+    std::vector<Fi> includedFactors_;
+    std::vector<Fi> includedFactorsStorageIndex_;
     Vi nVar_;
+    Fi nFac_;
+
+    
+
+    std::vector<FullyIncludedFactor> fullyIncludedBuffer_;
 };
 
 
--- src/test/model/test_view_submodel.cxx
+++ src/test/model/test_view_submodel.cxx
@@ -5,6 +5,7 @@
 
 #include "inferno/inferno.hxx"
 #include "inferno/model/general_discrete_model.hxx"
+#include "inferno/model/factors_of_variables.hxx"
 #include "inferno/model/view_submodel.hxx"
 
 
@@ -16,15 +17,48 @@ BOOST_AUTO_TEST_CASE(TestViewSubmodel)
     using namespace inferno;
 
     // create the base model
-    const auto shape = {20, 20};
+    const auto shape = {3, 3};
     const auto nl = 5;
     const auto beta = 0.1;
     const auto submodular = false;
-    const auto baseModel = test_models::grid2d(shape, nl, beta, submodular);
+
+    // create the base model
+    typedef GeneralDiscreteGraphicalModel Model;
+    typedef FactorsOfVariables<Model> FacOfVars;
+    const Model model = test_models::grid2d(shape, nl, beta, submodular);
+    const FacOfVars facOfVars(model);
 
     // create the view submodel
-    typedef models::ViewSubmodel<GeneralDiscreteGraphicalModel> ViewSubmodelType;
-    ViewSubmodelType submodel(baseModel);
+    typedef models::ViewSubmodel<Model, FacOfVars> ViewSubmodelType;
+    BOOST_TEST_CHECKPOINT("submodel constructor");
+    ViewSubmodelType submodel(model, facOfVars);
 
+    BOOST_TEST_CHECKPOINT("check nVariables");
+    BOOST_CHECK_EQUAL(submodel.nVariables(),0);
+    //BOOST_CHECK_EQUAL(submodel.nFactors(),0);
+    // 0 | 1 | 2 
+    // -- --- --
+    // 3 | 4 | 5
+    // -- --  --
+    // 6 | 7 | 8
+
+    BOOST_TEST_CHECKPOINT("get labels");
+    // set the states of the base model
+    auto & labels = submodel.baseModelLabelMap();
+
+    BOOST_TEST_CHECKPOINT("fill labels");
+    for(const auto vi : model.variableIds())
+        labels[vi] = 0;
+
+    //   | 1 |   
+    // -- --- --
+    //   | 4 | 5
+    // -- --  --
+    //   |   |  
+    BOOST_TEST_CHECKPOINT("setSubmodelVariables");
+    //submodel.setSubmodelVariables({1, 4, 5});
+    //BOOST_CHECK_EQUAL(submodel.nVariables(),3);
+
+    std::cout<<"end test\n";
 
 }
